d4c8d1aa3500bd8783dce67cfa1cfc62fd939935
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2021-11-06 21:18:55 +0100

    edit: simplified default page

--- App.js
+++ App.js
@@ -62,30 +62,17 @@ const App: () => Node = () => {
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View
+        style={{
+          backgroundColor: isDarkMode ? Colors.black : Colors.white,
+        }}>
+        <Section title="Section 1">
+          This is <Text style={styles.highlight}>Section 1</Text>.
+        </Section>
+        <Section title="Section 2">
+          This is Section 2.
+        </Section>
+      </View>
     </SafeAreaView>
   );
 };
