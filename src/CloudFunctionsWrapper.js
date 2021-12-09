4b5926b08013f5572edeab234c12810bd39994de
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2021-12-09 18:26:37 +0100

    edit: outsource function call boilerplate

--- src/CloudFunctionsWrapper.js
+++ src/CloudFunctionsWrapper.js
@@ -4,44 +4,51 @@ import Product from './store/Product';
 import Nutrition from './store/Nutrition';
 
 const functions = firebase.app().functions('europe-west1');
-async function getProductByGTIN(gtin) {
+
+function getProductByGTIN(gtin) {
+  return callCloudFunction('getProduct', {id: gtin}, response => {
+    const info = response.data.product;
+    const nutriInfo = info.nutriments;
+    let nutrition = new Nutrition(
+      nutriInfo['energy-kcal'],
+      nutriInfo.fat,
+      nutriInfo['saturated-fat'],
+      nutriInfo.carbohydrates,
+      nutriInfo.sugars,
+      nutriInfo.proteins,
+      nutriInfo.salt,
+    );
+    let product = new Product(
+      info.code,
+      info.product_name,
+      info.generic_name,
+      nutrition,
+      info.ingredients_text,
+      null,
+      info.nutriscore_data.grade,
+      [info.image_url],
+    );
+    console.log(product);
+    return product;
+  });
+}
+
+async function callCloudFunction(functionName, parameters, resolve) {
   return await functions
-    .httpsCallable('getProduct')({id: gtin})
+    .httpsCallable(functionName)(parameters)
     .then(response => {
       if (
         typeof response.data !== 'object' ||
         'httpErrorCode' in response.data
       ) {
-        console.error('fail getProductByGTIN with', gtin);
+        console.error(`fail ${functionName} with`, parameters);
         console.error(response.data);
         return;
       }
-      const info = response.data.product;
-      const nutriInfo = info.nutriments;
-      let nutrition = new Nutrition(
-        nutriInfo['energy-kcal'],
-        nutriInfo.fat,
-        nutriInfo['saturated-fat'],
-        nutriInfo.carbohydrates,
-        nutriInfo.sugars,
-        nutriInfo.proteins,
-        nutriInfo.salt,
-      );
-      let product = new Product(
-        info.code,
-        info.product_name,
-        info.generic_name,
-        nutrition,
-        info.ingredients_text,
-        null,
-        info.nutriscore_data.grade,
-        [info.image_url],
-      );
-      console.log(JSON.stringify(product));
-      return product;
+      return resolve(response);
     })
     .catch(reason => {
-      console.error('error getProductByGTIN with', gtin);
+      console.error(`error ${functionName} with`, parameters);
       console.error(reason);
     });
 }
