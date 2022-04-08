315efc0a48db84b065528bade2ada7e588868d6d
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2022-04-08 19:08:45 +0200

    add: user authentication (google sign-in, signout)

--- src/navigation/screens/SettingsScreen.js
+++ src/navigation/screens/SettingsScreen.js
@@ -3,23 +3,106 @@ import {View, Text, Button, StyleSheet, Image, ScrollView} from 'react-native';
 import {Avatar, ListItem, Icon} from 'react-native-elements';
 import Searchbar from '../../components/TextInput.component';
 import {LocalizationContext} from '../../components/Translations';
+import {AuthContext} from '../../components/Authentication';
+import auth from '@react-native-firebase/auth';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+
+const LoginView = () => {
+  const [signingIn, setSigningIn] = useState(false);
+  const {translations} = useContext(LocalizationContext);
+  const {user, setUser} = useContext(AuthContext);
+
+  const onGoogleSignIn = async () => {
+    setSigningIn(true);
+    console.log('Signing in with Google...');
+    try {
+      const {idToken} = await GoogleSignin.signIn();
+      console.log('ID-Token:');
+      console.log(idToken);
+      const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+      console.log('Google-Cred:');
+      console.log(googleCredential);
+      const userCredential = await auth().signInWithCredential(
+        googleCredential,
+      );
+      setUser(userCredential.user);
+      console.log('Signed in with Google!');
+      console.log(user);
+    } catch (ex) {
+      console.log('Error on Google Sign-in:');
+      console.log(ex);
+    }
+    setSigningIn(false);
+  };
+
+  const onGithubSignIn = () => {
+    return null;
+  };
+
+  const onLogout = () => {
+    console.log('Signing out...');
+    auth().signOut();
+    console.log('Signed out!');
+  };
+
+  if (!user) {
+    // show login buttons
+    return (
+      <View>
+        <GoogleSigninButton
+          style={{width: 192, height: 48}}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={onGoogleSignIn}
+          disabled={signingIn}
+        />
+        <Button
+          title={translations['settings.login.github']}
+          onPress={onGithubSignIn}
+        />
+      </View>
+    );
+  } else {
+    // show login info
+    return (
+      <View>
+        <Text style={styles.subTitle}>
+          {`${translations['settings.login.status']} ${user.providerId}: ${user.displayName} (${user.email})`}
+        </Text>
+        <Text style={styles.subTitle}>
+          {`${translations['settings.login.email_verified']} ${user.emailVerified}`}
+        </Text>
+        <Text style={styles.subTitle}>
+          {`${translations['settings.login.anonymous']} ${user.isAnonymous}`}
+        </Text>
+        <Button
+          title={translations['settings.login.logout']}
+          onPress={onLogout}
+        />
+      </View>
+    );
+  }
+};
 
 const SettingsScreen = () => {
   const [value, setValue] = useState();
+
   function updateSearch(val) {
     //search logic or anything
     console.log(val);
   }
+
   const {translations, appLanguage, setAppLanguage} =
     useContext(LocalizationContext);
+
   return (
     <View style={styles.container}>
       <Text style={styles.headline}>{translations['settings.login.text']}</Text>
 
-      <Text style={styles.username}>{translations['settings.username']}</Text>
-      <Searchbar value={value} updateSearch={updateSearch} />
-      <Text style={styles.password}>{translations['settings.password']}</Text>
-      <Searchbar value={value} updateSearch={updateSearch} />
+      {LoginView()}
 
       <View style={styles.topSeparator} />
 
